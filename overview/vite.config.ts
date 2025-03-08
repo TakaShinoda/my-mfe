cb20b192e5fb9024d5cd77d773707dc9d6463a53
Takahiro <tkhr_621@docomo.ne.jp>
2025-03-08 15:59:56 +0900

    add setting

--- overview/vite.config.ts
+++ overview/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  },
+  server: {
+    port: 5000,
+    strictPort: true
+  },
+  preview: {
+    port: 5000,
+    strictPort: true
+  }
 })
