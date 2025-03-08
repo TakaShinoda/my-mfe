8698deed3d52ce56dd87544f4b26fa41cf2d7aac
Takahiro <tkhr_621@docomo.ne.jp>
2025-03-08 17:31:33 +0900

    Card を使う

--- stats/src/components/Stats.tsx
+++ stats/src/components/Stats.tsx
@@ -1,10 +1,13 @@
+import { Card } from '@radix-ui/themes'
 import { ProgressBar } from './ProgressBar'
 
 export const Stats = () => {
   return (
-    <div className="st:pt-6">
-      <h2 className="st:text-xl st:font-semibold st:mb-4">ステータス</h2>
-      <ProgressBar />
-    </div>
+    <Card>
+      <div className="st:pt-6">
+        <h2 className="st:text-xl st:font-semibold st:mb-4">ステータス</h2>
+        <ProgressBar />
+      </div>
+    </Card>
   )
 }
