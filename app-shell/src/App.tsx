12af7a3cd01290ce2089ea4f1d4a00326c7f7584
Takahiro <tkhr_621@docomo.ne.jp>
2025-03-08 14:50:48 +0900

    install tailwind

--- app-shell/src/App.tsx
+++ app-shell/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1 className="text-5xl text-blue-500 font-bold ">Hello React</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
